7a6971fbd19bcc40a2e5d9a35df727203f067a09
Noruleez <normikp@gmail.com>
2023-06-05 16:53:50 +0700

    6_commit

--- accounts_shop/settings.py
+++ accounts_shop/settings.py
@@ -145,11 +145,11 @@ EMAIL_HOST_PASSWORD = 'ucdklwzfwmtdvnnm'
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static')
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
