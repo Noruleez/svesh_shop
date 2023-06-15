d8a3dcefb3ebf4fc0618301cdfd4a33e6711e01b
Noruleez <normikp@gmail.com>
2023-06-15 18:18:56 +0700

    payment_system_add_7

--- payment/views.py
+++ payment/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.shortcuts import get_object_or_404
 from django.views.generic import View
 from django.core.exceptions import ValidationError
+import hashlib
+from hashlib import md5
 
 from .forms import PaymentForm
 from .service import get_payment_link
@@ -29,8 +31,20 @@ class ChoosePaymentSystem(View):
 
 class QiwiPaymentSystem(View):
     def get(self, request):
-        form = PaymentForm()
-        return render(request, 'payment/qiwi_payment_system.html', context={'form': form})
+        merchant_id = '35421'
+        order_amount = '100'
+        currency = 'RUB'
+        order_id = '1'
+        secret_word = 'wrRI*,Y}nau9Z4O'
+        sign = md5(f'{merchant_id}:{order_amount}:{secret_word}:{currency}:{order_id}'.encode('utf-8')).hexdigest()
+        context = {
+            'm': merchant_id,
+            'oa': order_amount,
+            'o': order_id,
+            's': sign,
+            'currency': currency
+        }
+        return render(request, 'payment/qiwi_payment_system.html', context)
 
     def post(self, request):
         payment_system_id = 35
