2b9813a98d4b1b40ad369349030e4af19d299cd3
Noruleez <normikp@gmail.com>
2023-06-15 15:57:43 +0700

    payment_system_add_6

--- payment/forms.py
+++ payment/forms.py
@@ -5,7 +5,7 @@ from django.core.exceptions import ValidationError
 
 class PaymentForm(forms.ModelForm):
     class Meta:
-        # model = Balance
+        model = Balance
         fields = ['amount']
         widgets = {
             'amount': forms.TextInput(attrs={'class': 'form-control'}),
