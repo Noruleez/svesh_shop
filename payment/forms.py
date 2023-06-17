ae596a3378bc58da324552ff3a55126a42db0cbd
Noruleez <normikp@gmail.com>
2023-06-17 12:30:41 +0700

    payment_system_8

--- payment/forms.py
+++ payment/forms.py
@@ -3,3 +3,8 @@ from django import forms
 
 class PaymentForm(forms.Form):
     amount = forms.IntegerField(max_digits=8, decimal_places=2)
+    class Meta:
+        fields = ['amount']
+        widgets = {
+            'amount': forms.TextInput(attrs={'class': 'form-control'}),
+        }
