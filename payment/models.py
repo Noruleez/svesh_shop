8da34c45b103e89ebf0afd13f4070a516453b082
Noruleez <normikp@gmail.com>
2023-07-24 13:26:22 +0700

    add_aaio_payment_system_3

--- payment/models.py
+++ payment/models.py
@@ -7,7 +7,6 @@ from django.contrib.auth.models import User
 
 class FreeKassaPaymentStatus(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    order_id = models.DecimalField(max_digits=8, decimal_places=0, default=0, db_index=True)
     date_time_create = models.DateTimeField(auto_now_add=True)
     amount = models.DecimalField(max_digits=8, decimal_places=2, default=0, db_index=True)
     status = models.CharField(max_length=150, db_index=True)
@@ -18,7 +17,6 @@ class FreeKassaPaymentStatus(models.Model):
 
 class AaioPaymentStatus(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    order_id = models.DecimalField(max_digits=8, decimal_places=0, default=0, db_index=True)
     date_time_create = models.DateTimeField(auto_now_add=True)
     amount = models.DecimalField(max_digits=8, decimal_places=2, default=0, db_index=True)
     status = models.CharField(max_length=150, db_index=True)
