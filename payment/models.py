7c34eaef6f564ed7d2ca02aeb33ad4e0636a4262
Noruleez <normikp@gmail.com>
2023-08-13 16:08:37 +0700

    add_aaio_payment_system_4

--- payment/models.py
+++ payment/models.py
@@ -13,7 +13,7 @@ class FreeKassaPaymentStatus(models.Model):
 
 
 class AaioPaymentStatus(models.Model):
-    user = models.ForeignKey(User)
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     # user = models.ManyToManyField(User)
     date_time_create = models.DateTimeField(auto_now_add=True)
     amount = models.DecimalField(max_digits=8, decimal_places=2, default=0, db_index=True)
