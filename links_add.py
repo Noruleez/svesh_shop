71fbeccfde681bfc2a87a1ca66c3d596e503b816
Noruleez <normikp@gmail.com>
2023-08-14 17:42:29 +0700

    revision_1

---
+++ links_add.py
@@ -0,0 +1,15 @@
+from shop.models import *
+
+
+country = Country.objects.get(title='Индонезия')
+delay = 7
+product = Product.objects.get(country=country, delay=delay)
+
+links = []
+
+with open('links.txt', 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        link = line[:-1]
+        ProductLink.objects.create(product=product, link=link)
+
