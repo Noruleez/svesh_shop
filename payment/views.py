5671c6ff9b172a3484e1bb97d99c16072814cea4
Noruleez <normikp@gmail.com>
2023-08-15 11:12:13 +0700

    revision_2

--- payment/views.py
+++ payment/views.py
@@ -146,11 +146,11 @@ class AaioPaymentSystem(View):
                 except ValueError:
                     return False
 
-            if isint(new_form.amount) == False:
+            #if isint(new_form.amount) == False:
+            if True:
                 error_payment_integer_amount = 'Введите целое число'
                 return render(request, 'payment/aaio_payment_system.html', context={'error_payment_amount': error_payment_integer_amount,
                                                                                     'form': bound_form})
-
 
             if len(AaioPaymentStatus.objects.filter(user=request.user, status='WaitPayment')) == 1:
                 already_exists_payment = AaioPaymentStatus.objects.get(user=request.user, status='WaitPayment')
