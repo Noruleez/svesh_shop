5b1567438932cea09db7958457053448b5eea39e
Noruleez <normikp@gmail.com>
2023-09-29 17:15:40 +0700

    revision_8

--- shop/views.py
+++ shop/views.py
@@ -140,5 +140,7 @@ class PurchasesList(View):
 class PurchaseLinksList(View):
     def get(self, request, slug):
         purchase = Purchase.objects.get(slug=slug)
+        if purchase.user != request.user or request.user.is_anonymous:
+            return render(request, 'shop/purchase_links_list.html')
         links = PurchaseLink.objects.filter(purchase__id=purchase.id)
         return render(request, 'shop/purchase_links_list.html', context={'links': links})
