0974a4978b2f4597aae37f06c3d9c8ce9a3af67f
Noruleez <normikp@gmail.com>
2023-10-17 15:38:04 +0700

    revision_12

--- payment/admin.py
+++ payment/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import *
+from .models import AaioPaymentStatus, FreeKassaPaymentStatus
 
 admin.site.register(AaioPaymentStatus)
+admin.site.register(FreeKassaPaymentStatus)
